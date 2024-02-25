219988bded0213053b050043a1caaa6d3077a39d
CrimsonRobin <CrimsonRobin807@outlook.com>
2024-02-25 12:17:08 -0500

    Finished code for create

--- data/products.js
+++ data/products.js
@@ -1,4 +1,9 @@
 // TODO: Export and implement the following functions in ES6 format
+import { products } from "../config/mongoCollections.js";
+import * as helper from "../helpers.js";
+import {isValid, isMatch} from "date-fns";
+import {ObjectId} from "mongodb";
+
 const create = async (
   productName,
   productDescription,
@@ -11,7 +16,57 @@ const create = async (
   dateReleased,
   discontinued
 ) => {
+  productName = helper.checkIfString(productName);
+  productDescription = helper.checkIfString(productDescription);
+  modelNumber = helper.checkIfString(modelNumber);
+  manufacturer = helper.checkIfString(manufacturer);
+  manufacturerWebsite = helper.checkIfString(manufacturerWebsite);
+  dateReleased = helper.checkIfString(dateReleased);
+  price = helper.checkIfPriceValid(price);
+
+  if(manufacturerWebsite.toLowerCase().startsWith("http://www.") === false || manufacturerWebsite.toLowerCase().endsWith(".com") === false) {
+    throw new Error("given website does not start with http:/www. or ends in .com");
+  }
+  if(manufacturerWebsite.length < 20) {
+    throw new Error("given website is not at least 20 characters long.");
+  }
+
+  helper.checkIfValidArray(keywords);
+  helper.checkIfValidArray(categories);
   
+
+  if(isValid(new Date(dateReleased)) === false || isMatch(dateReleased, "MM/dd/yyyy")) {
+    throw new Error("date released is not a valid date.");
+  }
+
+  if(typeof discontinued !== "boolean") {
+    throw new Error("discontinued is not a boolean.");
+  }
+
+  let newProduct = {
+  productName: productName,
+  productDescription: productDescription,
+  modelNumber: modelNumber,
+  price: price,
+  manufacturer: manufacturer,
+  manufacturerWebsite: manufacturerWebsite,
+  keywords: keywords,
+  categories: categories,
+  dateReleased: dateReleased,
+  discontinued: discontinued
+  };
+
+  const productCollection = await products();
+  const insertInfo = await productCollection.insertOne(newProduct);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+      throw new Error("could not add product.");
+    }
+
+    const newId = insertInfo.insertedId.toString();
+
+    const product = await this.get(newId);
+    return product;
+
 };
 
 const getAll = async () => {};
--- helpers.js
+++ helpers.js
@@ -1,1 +1,43 @@
 // You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
+function checkIfString(str) {
+    if(!str) {
+        throw new Error("given string is not a string.");
+    }
+    if(typeof str !== "string") {
+        throw new Error("given string is not a string.");
+    }
+    str = str.trim();
+    if(str.length === 0) {
+        throw new Error("string is empty.");
+    }
+
+    return str;
+}
+
+function checkIfPriceValid(price) {
+    if(typeof price !== "number") {
+        throw new Error("given price is not a number.");
+    }
+    if(Number.isFinite(price) === false) {
+        throw new Error("given price is not a valid number.");
+    }
+    if(price <= 0) {
+        throw new Error("given price is not greater than 0.");
+    }
+    price = +price.toFixed(2);
+    return price;
+}
+
+function checkIfValidArray(arr) {
+    if(Array.isArray(arr) === false) {
+        throw new Error("given array is not an array.");
+    }
+    if(arr.length < 1) {
+        throw new Error("given array does not have at least one element.");
+    }
+    for(let x of arr) {
+        checkIfString(x);
+    }
+}
+
+export {checkIfString, checkIfPriceValid, checkIfValidArray}
